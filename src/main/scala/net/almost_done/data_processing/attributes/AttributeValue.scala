54f820e7e5dca3442213e44751d5adeee589bb2b
Jacek Królikowski <nietaki@gmail.com>
2014-06-26 16:49:58 +0200

    AttributeValue more compact

--- src/main/scala/net/almost_done/data_processing/attributes/AttributeValue.scala
+++ src/main/scala/net/almost_done/data_processing/attributes/AttributeValue.scala
@@ -5,7 +5,16 @@ package net.almost_done.data_processing.attributes
  *
  * wrapper base class for attribute values to make working on Int and Double histograms easier
  */
-abstract class AttributeValue {
-  val intVal: Int;
-  val doubleVal: Double;
+trait AttributeValue {
+  def intVal: Int;
+  def doubleVal: Double;
 }
+
+object AttributeValue {
+  def apply[N: Numeric](v: N) = new AttributeValue {
+    import Numeric.Implicits._ //TODO switch to spire Numeric here
+    def intVal = v.toInt()
+    def doubleVal = v.toDouble()
+  }
+}
+
--- src/main/scala/net/almost_done/data_processing/attributes/IntValue.scala
+++
@@ -1,8 +0,0 @@
-package net.almost_done.data_processing.attributes
-
-/**
- * Created by nietaki on 25.06.14.
- */
-case class IntValue(val intVal: Int) extends AttributeValue {
-  val doubleVal = ???
-}
